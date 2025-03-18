8aa66ca08bd47febbe873a372bd7825c48fbd9c1
Serious-Sam97 <samzuckerberg.jar@gmail.com>
2025-03-18 09:01:51 -0300

    Files grid done

--- src/components/Apps/Explorer.tsx
+++ src/components/Apps/Explorer.tsx
@@ -7,7 +7,21 @@ const Explorer: React.FC = () => {
         axios.get('http://localhost:8080/files');
     }, []);
     return (
-        <h1>Test</h1>
+        <div style={{width: '700px'}}>
+            <div style={{width: '100%'}}>
+            {
+                Array.from({length: 7}).map((_, iIndex) => (
+                    <div style={{display: 'flex', justifyContent: 'space-between', paddingLeft: '10px', paddingRight: '10px'}}>
+                        {
+                            Array.from({length: 7}).map((_, jIndex) => (
+                                <h2>File</h2>
+                            ))
+                        }
+                    </div>
+                ))
+            }
+            </div>
+        </div>
     )
 };
 
